3b35832ee3c09a55a2c896e5946766fdb7b7fabb
StarKayC <5252904+starkayc@users.noreply.github.com>
2024-08-29 21:09:01 -0400

    update all keyboards

---
+++ angel65/firmware_source/vial-qmk/cablecardesigns/ccd65/atmel/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA6, 0x7D, 0xBA, 0x5E, 0x9E, 0xC6, 0xC9, 0xCD}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 13}
---
+++ scarlet_tkl/firmware_source/vial-qmk/keyboards/lucid/scarlet/keymaps/vial_1000/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x71, 0x59, 0xE3, 0xAD, 0xE1, 0xE2, 0x92, 0x71}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 13}
+
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 12
